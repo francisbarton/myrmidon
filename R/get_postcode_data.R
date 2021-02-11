054b324a28edcb705f54fa50814e0f03e9430f8d
Francis Barton <fbarton@alwaysdata.net>
2021-02-11 22:28:36 +0000

    Made postcode lookup stop on error - warnings were not being made visible

--- R/get_postcode_data.R
+++ R/get_postcode_data.R
@@ -10,47 +10,77 @@
 #'
 get_postcode_data <- function(postcodes, return_style = c("moderate", "minimal", "full")) {
 
-  # calls to postcodes.io API and returns rows of data
-  retrieve_postcode_metadata <- function(postcodes) {
-    endpoint <- "https://api.postcodes.io/postcodes"
 
-    # session <- polite::bow(endpoint, force = TRUE)
+  # calls to postcodes.io API and returns rows of data
+  retrieve_postcode_metadata <- function(codes) {
+
 
     # replace NULL values with NA = thanks to
     # https://stackoverflow.com/a/49539022/5168907
     replace_x <- function(x, replacement = NA_character_) {
-      if (length(x) == 0 || length(x[[1]]) == 0) {
+      if (length(x) == 0 || length(x[[1]]) == 0 || is.null(x)) {
         replacement
       } else {
         x
       }
     }
 
-    out <- httr::POST(
-      url = endpoint
-      , body = list(postcodes = unique(unlist(postcodes)))
+
+
+    make_numeric <- function(lst) {
+      lst %>%
+        purrr::modify_at("eastings", ~ as.numeric(.)) %>%
+        purrr::modify_at("northings", ~ as.numeric(.)) %>%
+        purrr::modify_at("longitude", ~ as.numeric(.)) %>%
+        purrr::modify_at("latitude", ~ as.numeric(.))
+    }
+
+
+
+    endpoint <- "https://api.postcodes.io/postcodes"
+
+
+    out <- list(postcodes = codes) %>%
+      httr::POST(
+        url = endpoint
+      , body = .
       , encode = "json"
       # , config = httr::add_headers()
-      , httr::user_agent("Fran Barton github.com/francisbarton/myrmidon")
-      , httr::content_type_json()
+      # , httr::user_agent("Fran Barton github.com/francisbarton/myrmidon")
+      # , httr::content_type_json()
       )
+
     httr::warn_for_status(out)
 
-    result <- httr::content(out) %>%
-      purrr::pluck("result") %>%
-      purrr::map("result") %>%
-      purrr::modify_depth(2, replace_x)
 
-    info <- result %>%
+
+    results <- httr::content(out) %>%
+      purrr::pluck("result") %>%
+      purrr::modify_depth(3, replace_x) %>%
+      purrr::map("result") %>%
+      purrr::map(make_numeric)
+
+
+    info <- results %>%
       purrr::map_dfr(`[`, 1:23)
-    codes <- result %>%
+    codes <- results %>%
       purrr::map_dfr("codes") %>%
       dplyr::rename_with(~ paste0(., "_code"))
+
 
     dplyr::bind_cols(info, codes)
   }
 
+
+
+
+
+
+
   return_style <- match.arg(return_style, several.ok = FALSE)
+
+  if (is.null(return_style)) return_style <- "moderate"
+
 
   # tidy response and return
   if (return_style == "minimal") {
@@ -88,9 +118,45 @@ get_postcode_data <- function(postcodes, return_style = c("moderate", "minimal",
     cols_to_keep <- rlang::expr(dplyr::everything())
   }
 
-  postcodes %>%
-    myrmidon::batch_it(batches = 100, quiet = TRUE) %>%
+
+
+
+
+  postcodes <- postcodes[!is.na(postcodes)] %>%
+    unique()
+
+  fixed_postcodes <- postcodes %>%
+    validate_postcode_list()
+
+  if (!setequal(fixed_postcodes, postcodes)) {
+
+    usethis::ui_info(
+      paste0(
+        "Certain postcodes were found to be invalid ",
+        "and have been replaced with their nearest available ",
+        "valid postcode.\n",
+        "original codes: ",
+        stringr::str_c(
+          setdiff(fixed_postcodes, postcodes)
+          , collapse = ", "
+          ),
+        "\n",
+        "replacement codes: ",
+        stringr::str_c(
+          setdiff(postcodes, fixed_postcodes)
+          , collapse = ", "
+          )
+      )
+    )
+  }
+
+
+
+
+  fixed_postcodes %>%
+    myrmidon::batch_it(batches = 50, quiet = TRUE) %>%
     purrr::map_df(retrieve_postcode_metadata) %>%
-    # purrr::reduce(dplyr::bind_rows) %>%
     dplyr::select(!!cols_to_keep)
+
+
 }
