19e7e85ff5f6769b13156791906062a0bf52e22e
Fran Barton <fbarton@alwaysdata.net>
2022-06-11 18:44:51 +0100

    Quick script to get daily Welsh word of the day

---
+++ R/get_daily_welsh_word.R
@@ -0,0 +1,19 @@
+#' Gets the latest Welsh word of the day from the GPC (Geiridaur Prifysgol Cymru) Mastodon RSS feed
+#' @export
+get_daily_welsh_word <- function() {
+
+  feed_items <- feedeR::feed.extract("https://toot.wales/@geiriadurGPC.rss") %>%
+    purrr::pluck("items")
+
+  latest <- feed_items %>%
+    dplyr::slice(1) %>%
+    dplyr::pull(description)
+
+  latest %>%
+    rvest::read_html() %>%
+    rvest::html_elements("p") %>%
+    purrr::pluck(2) %>%
+    rvest::html_text() %>%
+    stringr::str_extract(".*(?=: https)") %>%
+    paste("Gair y Dydd /", .)
+}
