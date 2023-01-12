83287607655df9303cc98f2d331c6800039eef04
Fran Barton <fbarton@alwaysdata.net>
2023-01-12 08:09:16 +0000

    add 'project mascots' names lists

--- data-raw/project_mascots.R
+++ data-raw/project_mascots.R
@@ -65,3 +65,37 @@ project_mascots <- tibble::tibble(
       stringr::str_detect(x, "Face$") ~ stringr::str_remove(x, " Face$"),
       TRUE ~ x
     )))
+
+
+
+mascot_names1 <- "1fc6thrwFTPKwP0PlBTvzKjfiGlnM37JXLPaR8_In3fw" |>
+  googlesheets4::read_sheet(2, "C:C") |>
+  dplyr::pull(First) |>
+  unique() |>
+  stringr::str_to_sentence() |>
+  purrr::discard(
+    \(x) x %in% c(
+      "Alfre", "America", "Bigfreedia", "Blind", "Contrapoints", "Dj", "Dr", "Drake", "E", "Eazye", "Ego", "Ice", "J", "Kj", "Jesus", "Kasturba",
+      "Littlemix", "Ma", "Mc", "Mf", "Missmajor", "Mj", "M", "Ma", "Mf",
+      "Nate diana", "Nghi", "Nk", "P", "Pc", "Prince", "Pseudonym", "Rj",
+      "Scholastique", "Tanehisi", "Tnia", "Vv", "Yaa", "Yoshitsune", "Zapmama"
+    ))
+
+
+star_names <- "https://en.wikipedia.org/wiki/List_of_proper_names_of_stars" |>
+  rvest::read_html() |>
+  rvest::html_elements("table") |>
+  dplyr::nth(1) |>
+  rvest::html_table() |>
+  dplyr::pull(3) |>
+  stringr::str_extract("^[:alpha:]+") |>
+  unique() |>
+  stringr::str_to_sentence() |>
+  purrr::discard(
+    \(x) x %in% c(
+      "Aldebaran", "Absolutno", "Betelgeuse", "Felixvarela", "Intercrus", "La", "Lionrock", "Phact", "Pollux"
+      )) |>
+  purrr::discard(
+    \(x) nchar(x) > 9
+  )
+
