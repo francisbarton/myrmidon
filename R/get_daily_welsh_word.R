77807bf6b310c70429e62c4337d930b91b94a5a7
Fran Barton <francis.barton@nuh.nhs.uk>
2023-03-03 13:33:46 +0000

    Better version of dww

--- R/get_daily_welsh_word.R
+++ R/get_daily_welsh_word.R
@@ -5,14 +5,14 @@ get_daily_welsh_word <- function() {
   feed_items <- myrmidon::read_rss("https://toot.wales/@geiriadurGPC.rss") |>
     purrr::pluck("items")
 
-  latest <- feed_items |>
+  feed_items |>
     dplyr::slice(1) |>
     dplyr::pull(description) |>
     rvest::read_html() |>
     rvest::html_elements("p") |>
     purrr::pluck(2) |>
     rvest::html_text() |>
-    stringr::str_extract("(?<=^Word of the Day: )[^:]*(?= https)")
-
-  paste0("Gair y Dydd: ", latest)
+    stringr::str_squish() |>
+    stringr::str_replace("^Word of the Day", "Gair y Dydd") |>
+    stringr::str_extract(".+(?=: https://geiriadur)")
 }
