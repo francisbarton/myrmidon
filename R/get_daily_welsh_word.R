3e48bb3c14efef09128839d1ced75c8cc1236036
Fran Barton <francis.barton@nuh.nhs.uk>
2023-08-31 15:12:36 +0100

    Add more flexibility and accuracy to dww

--- R/get_daily_welsh_word.R
+++ R/get_daily_welsh_word.R
@@ -5,14 +5,25 @@ get_daily_welsh_word <- function() {
   feed_items <- myrmidon::read_rss("https://toot.wales/@geiriadurGPC.rss") |>
     purrr::pluck("items")
 
-  feed_items |>
-    dplyr::slice(1) |>
-    dplyr::pull(description) |>
-    rvest::read_html() |>
-    rvest::html_elements("p") |>
-    purrr::pluck(2) |>
-    rvest::html_text() |>
+  item_text <- feed_items |>
+    dplyr::pull("description") |>
+    purrr::map(rvest::read_html) |>
+    purrr::map(\(x) rvest::html_elements(x, "p")) |>
+    purrr::map(2) |>
+    purrr::compact() |>
+    purrr::map(rvest::html_text) |>
+    purrr::keep(\(x) stringr::str_starts(x, "Word of the Day")) |>
+    dplyr::first() |>
+    stringr::str_replace("^Word of the Day", "Gair y Dydd")
+
+  item_def <- stringr::str_extract(item_text, "^.*(?=:\\s+https)")
+  item_url <- stringr::str_extract(item_text, "https[:graph:]+(?=\\.)")
+  item_des <- stringr::str_remove(item_text, "^.*(?=:\\s+https)") |>
+    stringr::str_remove("^:\\s+") |>
+    stringr::str_remove("https[:graph:]*") |>
     stringr::str_squish() |>
-    stringr::str_replace("^Word of the Day", "Gair y Dydd") |>
-    stringr::str_extract(".+(?=: https://geiriadur)")
+    stringr::str_remove("^[:punct:]+") |>
+    stringr::str_squish()
+
+  list(item_def = item_def, item_url = item_url, item_des = item_des)
 }
