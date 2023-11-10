a496808710eabcad6a7801db2c6f6d7d23d72c27
Fran Barton <francis.barton@nuh.nhs.uk>
2023-11-10 23:44:17 +0000

    Edit / improve colours selection

--- data-raw/myr_colours.R
+++ data-raw/myr_colours.R
@@ -1,35 +1,25 @@
 myr_colours <- list(
-  blues = list(
-    "cadetblue",
-    "cyan4",
-    "darkcyan",
-    "deepskyblue",
-    "dodgerblue",
-    "lightskyblue",
-    "slateblue"
-  ),
-  reds = list(
-    "firebrick",
-    "deeppink",
-    "hotpink",
-    "maroon2",
-    "plum2",
-    "violetred"
-  ),
-  greens = list(
-    "seagreen",
-    "springgreen3",
-    "yellowgreen"
-  ),
-  misc = list(
-    "antiquewhite",
-    "bisque",
-    "coral",
-    "lavenderblush",
-    "lightcoral",
-    "papayawhip",
-    "slategrey",
-    "thistle"
-  )
+  "cadetblue",
+  "deepskyblue",
+  "dodgerblue",
+  "lightskyblue",
+  "slateblue4",
+  "hotpink1",
+  "maroon3",
+  "plum1",
+  "tomato1",
+  "violetred1",
+  "darkcyan",
+  "palegreen3",
+  "seagreen4",
+  "yellowgreen",
+  "bisque",
+  "coral",
+  "darkorchid",
+  "seashell",
+  "slategrey",
+  "thistle"
 ) |>
-  purrr::map(rlang::set_names)
+  rlang::set_names()
+
+usethis::use_data(myr_colours, overwrite = TRUE)
