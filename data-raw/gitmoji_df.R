9ff4d99f2a5f21dc17d309e5638a803d46e39034
Fran Barton <francis.barton@nuh.nhs.uk>
2023-11-10 22:56:12 +0000

    Add favourite colour names data (list)

--- data-raw/gitmoji_df.R
+++ data-raw/gitmoji_df.R
@@ -1,5 +1,6 @@
 gitmoji_df <- jsonlite::fromJSON("https://github.com/carloscuesta/gitmoji/raw/master/packages/gitmojis/src/gitmojis.json")[[1]]
 
 source(here::here("data-raw/project_mascots.R"))
+source(here::here("data-raw/myr_colours.R"))
 
-usethis::use_data(gitmoji_df, project_mascots, mascot_names, star_names, overwrite = TRUE, internal = TRUE)
+usethis::use_data(gitmoji_df, project_mascots, mascot_names, star_names, myr_colours, overwrite = TRUE, internal = TRUE)
---
+++ data-raw/myr_colours.R
@@ -0,0 +1,34 @@
+myr_colours <- list(
+  blues = list(
+    "cadetblue",
+    "cyan4",
+    "darkcyan",
+    "deepskyblue",
+    "dodgerblue",
+    "lightskyblue",
+    "slateblue"
+  ),
+  reds = list(
+    "firebrick",
+    "deeppink",
+    "hotpink",
+    "maroon2",
+    "plum2",
+    "violetred"
+  ),
+  greens = list(
+    "seagreen",
+    "springgreen3",
+    "yellowgreen"
+  ),
+  misc = list(
+    "antiquewhite",
+    "bisque",
+    "coral",
+    "lavenderblush",
+    "lightcoral",
+    "papayawhip",
+    "slategrey",
+    "thistle"
+  )
+)
