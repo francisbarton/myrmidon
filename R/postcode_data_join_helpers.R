cc7e2e08becb99623227a10137585a2eb709d188
Fran Barton <francis.barton@nuh.nhs.uk>
2024-03-26 00:55:49 +0000

    Fixes to address issue #8

--- R/postcode_data_join_helpers.R
+++ R/postcode_data_join_helpers.R
@@ -26,36 +26,106 @@ validate_code <- function(x) {
 
 
 
-check_term <- function(x) {
+check_terminated <- function(x) {
   req_base("terminated_") |>
     httr2::req_url_path_append(URLencode(x)) |>
     pluck_result()
 }
-check_term_possibly <- purrr::possibly(check_term, otherwise = NULL)
+check_terminated_possibly <- purrr::possibly(check_terminated, otherwise = NULL)
 
 
 
 
-bulk_reverse_geocode <- function(.data) {
-  req_base() |>
-    httr2::req_body_json(list(geolocations = .data)) |>
-    httr2::req_url_query(limit = 1) |>
-    pluck_result() |>
-    purrr::map_df("result")
+bulk_reverse_geocode <- function(.data, prev_data = NULL, curr_radius = 250L) {
+
+  if (curr_radius > 4000L) {
+    usethis::ui_info("Geocoding searches have not found some replacement postcodes despite searching up to 4km in radius from the original postcode. Stopping search now.")
+    prev_data # return early
+  }
+
+  # data_with_output_areas <- .data |>
+  #   dplyr::select(all_of(c(lon = "longitude", lat = "latitude"))) |>
+  #   dplyr::mutate(oa21cd = purrr::pmap_chr(lonlatapi::find_oa))
+
+  data_list <- .data |>
+    dplyr::select(all_of(c("longitude", "latitude"))) |>
+    dplyr::mutate(limit = 1L) |>
+    dplyr::mutate(radius = curr_radius) |>
+    dplyr::mutate(batch = ceiling(dplyr::row_number() / 100L)) |>
+    # batch into groups of max 100 rows
+    dplyr::group_split(batch, .keep = FALSE)
+
+  geodata_return <- data_list |>
+    purrr::map(get_geodata_return) |>
+    purrr::list_flatten() # re-combine batches into a single list
+
+  geodata_queries <- geodata_return |>
+    purrr::map("query")
+
+  geodata_query_results <- geodata_return |>
+    purrr::map("result") |>
+    purrr::list_flatten()
+
+  nifty_bind_cols <- function(x, y) {
+    x2 <- x |>
+      tibble::as_tibble_row() |>
+      dplyr::select(all_of(c("longitude", "latitude"))) |>
+      dplyr::rename_with(\(x) paste0("orig_", x))
+    if (!is.null(y)) {
+      y2 <- y |>
+        purrr::compact() |>
+        tibble::as_tibble() |>
+        unnest_codes()
+    } else {
+      y2 <- NULL
+    }
+    dplyr::bind_cols(x2, y2)
+  }
+
+  data_out <- geodata_queries |>
+    purrr::map2(geodata_query_results, nifty_bind_cols) |>
+    purrr::list_rbind()
+
+  if (!"postcode" %in% names(data_out)) {
+    data_out <- data_out |>
+      dplyr::mutate(postcode = NA_character_)
+  }
+
+  dat_done <- data_out |>
+    dplyr::filter(!if_any("postcode", is.na)) |>
+    dplyr::bind_rows(prev_data)
+
+  dat_missing <- data_out |>
+    dplyr::filter(if_any("postcode", is.na)) |>
+    dplyr::select(all_of(c(
+      longitude = "orig_longitude",
+      latitude = "orig_latitude"
+    )))
+
+  if (nrow(dat_missing) > 0L) {
+    bulk_reverse_geocode(dat_missing, dat_done, curr_radius = curr_radius * 2L)
+  } else {
+    dat_done |>
+      # https://stackoverflow.com/a/76044270#76044270
+      dplyr::rename_with(\(x) paste0("new_", x, recycle0 = TRUE), .cols = any_of(c("longitude", "latitude")))
+  }
 }
 
 
 
 
 unnest_codes <- function(.data) {
-  .data |>
-    dplyr::mutate(codes_names = names(codes)) |>
-    dplyr::mutate(across("codes", unlist)) |>
-    dplyr::rename(code = "codes") |>
-    tidyr::pivot_wider(
-      names_from = "codes_names",
-      names_glue = "{codes_names}_{.value}",
-      values_from = "code")
+  if ("codes" %in% names(.data)) {
+    .data |>
+      dplyr::mutate(codes_names = names(codes)) |>
+      dplyr::mutate(across("codes", unlist)) |>
+      dplyr::rename(code = "codes") |>
+      tidyr::pivot_wider(
+        names_from = "codes_names",
+        names_glue = "{codes_names}_{.value}",
+        values_from = "code"
+      )
+  } else .data
 }
 
 
@@ -69,12 +139,12 @@ autocomplete <- function(x) {
   req_base() |>
     httr2::req_url_path_append(x) |>
     httr2::req_url_path_append("/autocomplete") |>
-    httr2::req_url_query(limit = 1) |>
+    httr2::req_url_query(limit = 5L) |>
     pluck_result() |>
     unlist() |>
-    sample(1)
+    sample(1L)
 }
-autocomplete_possibly <- purrr::possibly(autocomplete, otherwise = NULL)
+autocomplete_possibly <- purrr::possibly(autocomplete, otherwise = NA)
 
 
 
@@ -84,4 +154,13 @@ bulk_lookup <- function(x) {
     httr2::req_body_json(list(postcodes = x), auto_unbox = FALSE) |>
     pluck_result() |>
     purrr::map_df("result")
+}
+
+
+get_geodata_return <- function(.data) {
+  req_base() |>
+    httr2::req_body_json(list(geolocations = .data)) |>
+    httr2::req_perform() |>
+    httr2::resp_body_json() |>
+    purrr::pluck("result")
 }
