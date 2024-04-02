15589c6b0c997577076958defcb65d9182f1df90
Fran Barton <francis.barton@nuh.nhs.uk>
2024-04-02 01:06:57 +0100

    Update internal package data

--- data-raw/gitmoji_df.R
+++ data-raw/gitmoji_df.R
@@ -1,5 +1,18 @@
-gitmoji_df <- jsonlite::fromJSON("https://github.com/carloscuesta/gitmoji/raw/master/packages/gitmojis/src/gitmojis.json")[[1]]
+gitmoji_df <- paste0(
+  "https://github.com/carloscuesta/gitmoji/",
+  "raw/master/packages/gitmojis/src/gitmojis.json"
+ ) |>
+  jsonlite::fromJSON() |>
+  purrr::pluck("gitmojis") |>
+  tibble::as_tibble()
 
 source(here::here("data-raw/project_mascots.R"))
 
-usethis::use_data(gitmoji_df, project_mascots, mascot_names, star_names, overwrite = TRUE, internal = TRUE)
+usethis::use_data(
+  gitmoji_df,
+  project_mascots,
+  mascot_names,
+  star_names,
+  overwrite = TRUE,
+  internal = TRUE
+)
