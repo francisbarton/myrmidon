b07399e105fa7fd62df54935435e71dd23ac7d21
Fran Barton <francis.barton@nhs.net>
2024-09-10 03:50:02 +0100

    Namespace stuff

--- R/myrmidon-package.R
+++ R/myrmidon-package.R
@@ -15,6 +15,7 @@
 #' @importFrom dplyr across desc first if_any if_else pick
 #' @importFrom ggplot2 element_blank element_line element_rect element_text 
 #' @importFrom ggplot2 margin
+#' @importFrom grDevices rgb col2rgb
 #' @importFrom rlang `:=` .data
 #' @importFrom tidyselect all_of any_of ends_with everything
 #' @importFrom usethis ui_info ui_stop ui_oops ui_nope ui_code
