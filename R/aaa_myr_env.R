d8e6fb2a03ec896000b8026a00efd07a2e7b3700
Fran Barton <francis.barton@nhs.net>
2024-09-10 03:23:41 +0100

    Store myr_prompt data in package env instead of creating vars in R profile

--- R/aaa_myr_env.R
+++ R/aaa_myr_env.R
@@ -1,1 +1,17 @@
 .myr_env <- rlang::new_environment()
+
+populate_myr_env <- function() {
+  moon_phase <- if (require(suncalc, quietly = TRUE)) {
+    getMoonIllumination()[["phase"]]
+  } else NULL
+  # some variables that myrmidon::myr_prompt_*() can use
+  rlang::env_bind_lazy(
+    .myr_env,
+    # time when R session started
+    start_time = Sys.time(),
+    moon_phase = moon_phase,
+    myr_prompt_col1 = "whitesmoke",
+    myr_prompt_col2 = "orange",
+    myr_prompt_col3 = "darkseagreen1"
+  )
+}
