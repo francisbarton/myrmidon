f3c0d6b279659fd715d6395a88d3e69177a00457
Fran Barton <fbarton@alwaysdata.net>
2025-03-06 20:12:55 +0000

    Delete R/flowview.R

--- R/flowview.R
+++
@@ -1,9 +0,0 @@
-#' @export
-flowview <- function() {
-  lst <- rstudioapi::getSourceEditorContext()
-  doc_id <- lst[["id"]]
-  cur_row <- lst[["selection"]][[1]][["range"]][["start"]][["row"]]
-  row_start <- rstudioapi::document_position(cur_row, 1)
-  rstudioapi::insertText(row_start, "  View() # ", doc_id)
-  invisible(TRUE)
-}
